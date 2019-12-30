07d40b58b3cfe194277115023056c2bb5deab089
Janae Hall <janaeelisa@gmail.com>
2019-12-30 12:04:18 -0600

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,29 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case "UPVOTE_QUOTE":
+      return state.map(quote => {
+        return quote.id === action.quoteId? {...quote, votes: quote.votes + 1}
+        : quote
+      })
+
+    case "DOWNVOTE_QUOTE":
+      let thisQuote = state.find(quote => quote.id === action.quoteId)
+      if (thisQuote.votes > 0){
+        return state.map(quote => {
+          return quote === thisQuote ? {...quote, votes: quote.votes - 1}
+          : quote
+        })
+      }
+      return state
+
+    default:
+      return state
+  }
+  // return state;
 }
